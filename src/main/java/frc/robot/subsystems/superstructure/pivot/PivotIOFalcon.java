0ae979aadbbad86763d78b9b3603c1c23c9f7386
Aidan Somerville <aidan21122@gmail.com>
2025-01-11 15:54:06 -0500

    Convert to angles p2

--- src/main/java/frc/robot/subsystems/superstructure/pivot/PivotIOFalcon.java
+++ src/main/java/frc/robot/subsystems/superstructure/pivot/PivotIOFalcon.java
@@ -7,7 +7,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
 
 public class PivotIOFalcon implements PivotIO {
     private final TalonFX leftMotor = new TalonFX(0);
