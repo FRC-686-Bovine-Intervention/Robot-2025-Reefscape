28881c4165736443418eafe20646bbd93940545c
LightspeedLazer <michael@the-sims-family.net>
2025-01-18 13:10:39 -0500

    gear ratios

--- src/main/java/frc/robot/subsystems/superstructure/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/superstructure/elevator/ElevatorConstants.java
@@ -4,14 +4,24 @@ import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.units.measure.Distance;
+import frc.util.mechanismUtil.GearRatio;
 
 public class ElevatorConstants {
     
-    public static final Distance sprocketRadius = Inches.of(1.5);
+    public static final Distance sprocketRadius = Inches.of(1.432).div(2);
 
     public static final Distance pivotOffset = Meters.of(0.050800);
 
     public static final Distance minimumHeight = Inches.of(26.500000);
     public static final Distance maximumHeight = Inches.of(76.930235);
     public static final Distance maximumLength = maximumHeight.minus(maximumHeight);
+
+    public static final GearRatio motorToMechanism = new GearRatio()
+        .planetary(1.0/9.0)
+    ;
+    public static final GearRatio sensorToMechanism = new GearRatio()
+        .gear(10)
+        .gear(40)
+        .axle()
+    ;
 }
--- src/main/java/frc/robot/subsystems/superstructure/pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/superstructure/pivot/PivotConstants.java
@@ -6,6 +6,7 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.units.measure.Distance;
+import frc.util.mechanismUtil.GearRatio;
 
 public class PivotConstants {
     public static final Distance pivotX = Meters.of(-0.228600);
@@ -19,4 +20,18 @@ public class PivotConstants {
         ),
         Rotation3d.kZero
     );
+
+    public static final GearRatio motorToMechanism = new GearRatio()
+        .planetary(1.0/5.0)
+        .planetary(1.0/5.0)
+        .gear(20)
+        .gear(66)
+        .axle()
+        .gear(10)
+        .gear(120)
+        .axle()
+    ;
+    public static final GearRatio sensorToMechanism = new GearRatio()
+        
+    ;
 }
