314fc860f42db242d6ebcb872c257048585bf1cb
Ladle <77344665+The-Ladle@users.noreply.github.com>
2025-02-21 19:06:12 -0500

    WIP ScoreCoral Auto Commands

--- src/main/java/frc/robot/auto/ScoreCoral.java
+++ src/main/java/frc/robot/auto/ScoreCoral.java
@@ -1,5 +1,6 @@
 package frc.robot.auto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.IntFunction;
@@ -8,9 +9,11 @@ import java.util.stream.IntStream;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
+import frc.robot.auto.AutoCommons.AutoPaths;
 import frc.robot.auto.AutoRoutine.AutoQuestion.Settings;
 import frc.robot.constants.FieldConstants;
 import frc.robot.constants.FieldConstants.Reef.Pipe;
+import frc.robot.subsystems.drive.Drive;
 import frc.util.misc.MathExtraUtil;
 
 public class ScoreCoral extends AutoRoutine {
@@ -22,7 +25,7 @@ public class ScoreCoral extends AutoRoutine {
     
     private static final Map.Entry<String, Pipe>[] pipeOptions =
         IntStream.range(0, FieldConstants.Reef.pipes.length)
-            .mapToObj(i -> Settings.option("Pipe " + (char) ('A' + i), FieldConstants.Reef.pipes[i]))
+            .mapToObj(i -> Settings.option("Pipe " + getBranchLetterFromIndex(i), FieldConstants.Reef.pipes[i]))
             .toArray((IntFunction<Map.Entry<String, Pipe>[]>) Map.Entry[]::new);
 
     private static boolean isRightCoralStation(Pipe pipe){
@@ -136,12 +139,65 @@ public class ScoreCoral extends AutoRoutine {
         }
     };
 
+    private final Drive drive;
     public ScoreCoral(RobotContainer robot) {
         super("ScoreCoral", List.of(scorePreloadPipe, startPosition, scoreCoral1, scoreCoral2, stationPosition));
+        this.drive = robot.drive;
     }
 
+    
     @Override
     public Command generateCommand() {
-        return Commands.none();
+        var _scorePreloadPipe = scorePreloadPipe.getResponse();
+        var _startPosition = startPosition.getResponse();
+        var _scoreCoral1 = scoreCoral1.getResponse();
+        var _scoreCoral2 = scoreCoral2.getResponse();
+        var _stationPosition = stationPosition.getResponse();
+        var commands = new ArrayList<Command>(1);
+
+        var startToScorePreload = AutoPaths.loadChoreoTrajectory(switch (_startPosition){
+            case CLOSE -> "Start To " + getBranchLetterFromIndex(_scorePreloadPipe.getIndex());
+            case REMOTE -> "Remote Start To " + getBranchLetterFromIndex(_scorePreloadPipe.getIndex());
+            default -> "Start To " + getBranchLetterFromIndex(_scorePreloadPipe.getIndex());
+        });
+        commands.add(drive.followBluePath(startToScorePreload));
+        var preloadToStation = AutoPaths.loadChoreoTrajectory(
+            getBranchLetterFromIndex(_scorePreloadPipe.getIndex())+
+            " To Station "+
+            getStationPositionAsString(_stationPosition)
+        );
+        commands.add(drive.followBluePath(preloadToStation));
+        var stationToScore1 = AutoPaths.loadChoreoTrajectory(
+            "Station "
+            + getStationPositionAsString(_stationPosition)
+            + " To "
+            + getBranchLetterFromIndex(_scoreCoral1.getIndex())
+        );
+        commands.add(drive.followBluePath(stationToScore1));
+        var coral1ToStation = AutoPaths.loadChoreoTrajectory(
+            getBranchLetterFromIndex(_scoreCoral1.getIndex())+
+            " To Station "+
+            getStationPositionAsString(_stationPosition)
+        );
+        commands.add(drive.followBluePath(coral1ToStation));
+        var stationToScore2 = AutoPaths.loadChoreoTrajectory(
+            "Station "
+            + getStationPositionAsString(_stationPosition)
+            + " To "
+            + getBranchLetterFromIndex(_scoreCoral2.getIndex())
+        );
+        commands.add(drive.followBluePath(stationToScore2));
+        return AutoCommons.setOdometryFlipped(null, drive);
+    }
+
+    private static char getBranchLetterFromIndex(int index){
+        return (char) (index+'A');
+    }
+    private String getStationPositionAsString(CoralStationPosition _stationPosition){
+        return switch(_stationPosition){
+            case CLOSE -> "Close";
+            case MID -> "Mid";
+            case FAR -> "Far";
+            default -> null;};
     }
 }
