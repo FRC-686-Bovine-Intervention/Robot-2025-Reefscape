58b7d7d6ceb465553dd90e2712821e2bf9b04fef
Ladle <77344665+The-Ladle@users.noreply.github.com>
2025-02-28 19:46:44 -0500

    DrivePastLine work-in-progress

--- src/main/java/frc/robot/auto/DrivePastLine.java
+++ src/main/java/frc/robot/auto/DrivePastLine.java
@@ -3,18 +3,21 @@ package frc.robot.auto;
 import java.util.List;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.drive.Drive;
 
 public class DrivePastLine extends AutoRoutine{
-
+    
+    private final Drive drive;
     public DrivePastLine(RobotContainer robot){
         super("DrivePastLine", List.of());
-    }
-    @Override
-    public Command generateCommand() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generateCommand'");
+        this.drive = robot.drive;
     }
 
-    
+
+    @Override
+    public Command generateCommand() {
+        return Commands.run(() -> drive.runRobotSpeeds(null), drive.translationSubsystem, drive.rotationalSubsystem).raceWith(Commands.waitSeconds(0.5));
+    }
 }
