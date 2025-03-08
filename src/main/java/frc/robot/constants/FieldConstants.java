6aed3983a521fa8bcd726a6c76ade12887fd31f9
LightspeedLazer <michael@the-sims-family.net>
2025-03-08 18:33:33 -0500

    move l4 back

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -243,10 +243,10 @@ public final class FieldConstants {
                     SuperstructureState.fromRobotSpace(
                         new Pose2d(
                             new Translation2d(
-                                RobotConstants.centerToFrontBumper.minus(Coral.length).minus(Inches.of(0)),
+                                RobotConstants.centerToFrontBumper.minus(Coral.length).minus(Inches.of(2)),
                                 Meters.of(1.828663).plus(Inches.of(15))
                             ),
-                            Rotation2d.fromDegrees(-25)
+                            Rotation2d.fromDegrees(-30)
                         )
                     )
                 )
