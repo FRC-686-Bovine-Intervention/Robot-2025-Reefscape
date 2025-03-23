d0c614ade49a6581cd7107e40b00a041b02720a2
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2025-03-23 09:48:41 -0400

    angled quest

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -120,8 +120,8 @@ public final class VisionConstants {
             // Inches.of(9.5),
             // Inches.zero()
             // -0.2431546438240109, -0.22893749281190218,0
-            0.24807059617995136, 0.22519556696985554, 0
+            // 0.24807059617995136, 0.22519556696985554, 0
         ),
-        new Rotation3d(Degrees.zero(), Degrees.zero(), Degrees.of(90))
+        new Rotation3d(Degrees.zero(), Degrees.zero(), Degrees.of(90)).rotateBy(new Rotation3d(Degrees.zero(), Degrees.of(-19), Degrees.zero()))
     ));
 }
