dd2a075a959a5094270aa5ce05dfbffe8d8fdc13
LightspeedLazer <michael@the-sims-family.net>
2025-04-04 14:26:22 -0400

    moved l4 closer and decreased auto delay

--- src/main/java/frc/robot/auto/AutoCommons.java
+++ src/main/java/frc/robot/auto/AutoCommons.java
@@ -79,7 +79,7 @@ public class AutoCommons {
                 Commands.sequence(
                     Commands.waitUntil(() -> superstructure.getCurrentState().isNear(targetState, Degrees.of(2), Inches.of(1), Degrees.of(5))),
                     Commands.waitUntil(() -> GeomUtil.isNear(end, drive.getPose(), Inches.of(5), Degrees.of(5))),
-                    Commands.waitSeconds(0.75),
+                    Commands.waitSeconds(0.25),
                     intake.eject().asProxy().onlyWhile(intake.hasCoral)
                 ),
                 Commands.sequence(
@@ -104,7 +104,7 @@ public class AutoCommons {
             Commands.sequence(
                 Commands.waitUntil(() -> superstructure.getCurrentState().isNear(targetState, Degrees.of(2), Inches.of(1), Degrees.of(5))),
                 Commands.waitUntil(() -> GeomUtil.isNear(end, drive.getPose(), Inches.of(5), Degrees.of(5))),
-                Commands.waitSeconds(0.75),
+                Commands.waitSeconds(0.5),
                 intake.eject().asProxy().onlyWhile(intake.hasAlgae)
             ),
             Commands.sequence(
