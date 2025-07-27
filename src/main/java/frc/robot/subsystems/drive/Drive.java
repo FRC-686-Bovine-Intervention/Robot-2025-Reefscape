7f279678954807b2a3718357e2fd331e0e12b3ef
Michael Sims <michael@the-sims-family.net>
2025-07-26 20:06:16 -0400

    linear limit doesn't falsely apply to rotation

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -246,7 +246,7 @@ public class Drive extends VirtualSubsystem {
         Logger.recordOutput("Drive/Swerve States/Setpoints Optimized", this.setpointStates);
     }
 
-    private static final LoggedTunableMeasure<LinearAccelerationUnit> forwardAccelLimitTunable = new LoggedTunableMeasure<>("Drive/Forward Accel Limit", MetersPerSecondPerSecond.of(50));
+    private static final LoggedTunableMeasure<LinearAccelerationUnit> forwardAccelLimitTunable = new LoggedTunableMeasure<>("Drive/Forward Accel Limit", MetersPerSecondPerSecond.of(500));
     private static final LoggedTunableMeasure<LinearAccelerationUnit> linearAccelLimitTunable = new LoggedTunableMeasure<>("Drive/Linear Accel Limit", MetersPerSecondPerSecond.of(10));
     public void runRobotSpeeds(ChassisSpeeds robotSpeeds) {
         this.desiredRobotSpeeds = robotSpeeds;
@@ -257,6 +257,8 @@ public class Drive extends VirtualSubsystem {
         Logger.recordOutput("Drive/Chassis Speeds/Desired Delta", desiredDelta);
         Logger.recordOutput("Drive/Chassis Speeds/Desired Accel", desiredAccel);
 
+        var limitedAccel = desiredAccel;
+
         // Forward Accel Limit
         var maxMeasuredModuleSpeed = Math.hypot(this.robotMeasuredSpeeds.vxMetersPerSecond, this.robotMeasuredSpeeds.vyMetersPerSecond) + Math.abs(this.robotMeasuredSpeeds.omegaRadiansPerSecond * DriveConstants.driveBaseRadius.in(Meters));
         var maxDesiredModuleAccel = Math.hypot(desiredAccel.vxMetersPerSecond, desiredAccel.vyMetersPerSecond) + Math.abs(desiredAccel.omegaRadiansPerSecond * DriveConstants.driveBaseRadius.in(Meters));
@@ -266,21 +268,25 @@ public class Drive extends VirtualSubsystem {
         Logger.recordOutput("Drive/Chassis Speeds/Forward Limit/Max Desired Module Accel", maxDesiredModuleAccel);
         Logger.recordOutput("Drive/Chassis Speeds/Forward Limit/Forward Accel Limit", forwardAccelLimit);
         Logger.recordOutput("Drive/Chassis Speeds/Forward Limit/Limiting Factor", forwardAccelLimitingFactor);
+        limitedAccel = limitedAccel.times(forwardAccelLimitingFactor);
 
         // Linear Accel Limit
         var desiredLinearAccel = Math.hypot(desiredAccel.vxMetersPerSecond, desiredAccel.vyMetersPerSecond);
         var linearAccelLimit = linearAccelLimitTunable.get().in(MetersPerSecondPerSecond);
         var linearAccelLimitingFactor = linearAccelLimit / Math.max(desiredLinearAccel, linearAccelLimit);
         Logger.recordOutput("Drive/Chassis Speeds/Linear Limit/Desired Linear Accel", desiredLinearAccel);
-        Logger.recordOutput("Drive/Chassis Speeds/Linear Limit/Linear Accel Limit", forwardAccelLimit);
-        Logger.recordOutput("Drive/Chassis Speeds/Linear Limit/Limiting Factor", forwardAccelLimitingFactor);
+        Logger.recordOutput("Drive/Chassis Speeds/Linear Limit/Linear Accel Limit", linearAccelLimit);
+        Logger.recordOutput("Drive/Chassis Speeds/Linear Limit/Limiting Factor", linearAccelLimitingFactor);
+        limitedAccel = new ChassisSpeeds(
+            limitedAccel.vxMetersPerSecond * linearAccelLimitingFactor,
+            limitedAccel.vyMetersPerSecond * linearAccelLimitingFactor,
+            limitedAccel.omegaRadiansPerSecond
+        );
 
 
-        var limitingFactor = Math.min(forwardAccelLimit, linearAccelLimitingFactor);
-        var limitedAccel = desiredAccel.times(limitingFactor);
         var limitedDelta = limitedAccel.times(RobotConstants.rioUpdatePeriodSecs);
         var limitedSpeeds = this.robotMeasuredSpeeds.plus(limitedDelta);
-        Logger.recordOutput("Drive/Chassis Speeds/Limiting Factor", limitingFactor);
+        // Logger.recordOutput("Drive/Chassis Speeds/Limiting Factor", limitingFactor);
         Logger.recordOutput("Drive/Chassis Speeds/Limited Accel", limitedAccel);
         Logger.recordOutput("Drive/Chassis Speeds/Limited Delta", limitedDelta);
         Logger.recordOutput("Drive/Chassis Speeds/Limited Speed", limitedSpeeds);
