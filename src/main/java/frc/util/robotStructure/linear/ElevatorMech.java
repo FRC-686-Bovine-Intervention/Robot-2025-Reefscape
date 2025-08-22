6a2110b9aad35ae456295bedd4c37681ec5efeff
Michael Sims <michael@the-sims-family.net>
2025-08-22 18:27:24 -0400

    raw doubles for mechanism3d
    added methods to set mechanism3d components using doubles

--- src/main/java/frc/util/robotStructure/linear/ElevatorMech.java
+++ src/main/java/frc/util/robotStructure/linear/ElevatorMech.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.numbers.N3;
 
 public class ElevatorMech extends LinearMech {
     private static final Vector<N3> axis = VecBuilder.fill(0,0,1);
+    
     public ElevatorMech(Transform3d base) {
         super(base, axis);
     }
